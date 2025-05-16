1b68aa837c6683b8cd039d50cb64e44b8d446378
landondelgado <landon.delgado@gmail.com>
2025-05-16 14:37:05 -0500

    Refactore For Render

--- server.js
+++ server.js
@@ -51,6 +51,12 @@ app.get('/agencydata', async (req, res) => {
     }
 });
 
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+app.use(express.static(path.join(__dirname, 'billingsoftware/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'billingsoftware/build/index.html'));
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
